a00903ddf8b3d891f53c5d2279f44d85d4010a4d
nathvictoria <victorianath06@gmail.com>
2024-04-10 10:30:44 -0300

    atividade - 10/04

--- helloWorld.js
+++ helloWorld.js
@@ -1,13 +1,13 @@
-const readline = require ('readline');
+const readline = require ('readline'); // importou um componente do nodejs
 
-const rl = readline.createInterface({
-    input: process.stdin,
+const rl = readline.createInterface({ //declarei a variável; chamei o componente e inicei a função
+    input: process.stdin, 
     output: process.stdout
     
-});
+}); 
 
-rl.question('Qual é o seu nome?', (nome) => {
-    console.log ('Olá, ${nome}! Bem vindo aoa Node.js');
-    rl.close();
+rl.question('Qual é o seu nome?', (nome) => { //vai ler a informação e guardar
+    console.log (`Olá, ${nome}! Bem vindo ao Node.js`); //vai apenas mostrar a informação; (utiliza-se crase em vez de aspas)
+    rl.close();// fechou a função que chamei lá em cima
 
 });
